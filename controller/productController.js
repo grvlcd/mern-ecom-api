9c0aca8422591b6d95ece07489802e95b2573cea
Christian <generalpride@gmail.com>
2024-04-13 21:57:11 +0800

    update response data

--- controller/productController.js
+++ controller/productController.js
@@ -2,25 +2,20 @@ import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
 const getProducts = asyncHandler(async (req, res) => {
-	const products = await Product.find();
+  const products = await Product.find();
 
-	res.json({
-		'data': products
-	});
+  res.json(products);
 });
 
 const getProductByid = asyncHandler(async (req, res) => {
-	const { id } = req.params;
-	const product = await Product.findOne({ _id: id });
-	console.log(product);
-	if (!product) {
-		throw new Error('Resource not found');
-	}
+  const { id } = req.params;
+  const product = await Product.findOne({ _id: id });
+  console.log(product);
+  if (!product) {
+    throw new Error("Resource not found");
+  }
 
-	res.json({
-		'data': product
-	});
+  res.json(product);
 });
-
 
 export { getProducts, getProductByid };
